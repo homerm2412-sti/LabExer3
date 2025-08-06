d3c3d594468e6268e56a3b9e64ef6412f4a1cb20
homerm2412-sti <homerm2412@gmail.com>
2025-08-06 17:13:07 +0800

    Add files via upload

---
+++ LabExer3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labexer3;
+
+/**
+ *
+ * @author PC1
+ */
+public class LabExer3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
